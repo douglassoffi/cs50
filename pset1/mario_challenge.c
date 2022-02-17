cf2b0d1cbb1dc937d3a1c764d7510e2384cbbc30
douglassoffi <douglas.soffi@protonmail.com>
2022-02-17 15:12:00 -0300

    add exercicios

---
+++ pset1/mario_challenge.c
@@ -0,0 +1,41 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1 || n > 8);
+
+    for (int x = 0; x < n; x++)
+    {
+        for (int y = 0; y < n; y++)
+        {
+            if (n - x - 2 < y)
+            {
+                printf("#");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("  ");
+
+        for (int z = 0; z < n; z++)
+        {
+            if (n - x - 2 < z)
+            {
+                printf("#");
+            }
+            else
+            {
+                printf("");
+            }
+        }
+        printf("\n");
+    }
+}
